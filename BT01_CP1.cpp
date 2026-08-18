52c253a91bfdf424941c45f1ea4dfea6384bd4f1
76DCHT21304-85-Phamtramy <tramypham28102007@gmail.com>
2026-08-18 13:06:27 +0700

    Create BT01_CP1.cpp

---
+++ BT01_CP1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct NhanVien {
+	string manv;
+	string tennv;
+	long luongcb;
+	int heso;
+};
+
+int main (){
+	NhanVien nv[100];
+	int n;
+	
+	cout << "Nhap so luong sinh vien: ";
+	cin >> n;
+}
